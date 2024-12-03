b3c70ca4a079294334e25014338195f9d8cec218
Judge Hiciano <BrainMonkeyInc@gmail.com>
2024-12-03 04:30:49 +0000

    use sorted filter

--- judge/day2.py
+++ judge/day2.py
@@ -1,6 +1,7 @@
 DAY_2_INPUT_FILE = "/Users/bm/repos/advent/data/day_2_input.txt"
 
 count = 0
+tmp_count=0
 with open(DAY_2_INPUT_FILE, "r") as f:
     
     text = f.readlines()
@@ -9,33 +10,22 @@ with open(DAY_2_INPUT_FILE, "r") as f:
         str_tmp_input = line.split("\n")[0].split(" ")
         tmp_input = [int(x) for x in str_tmp_input]
         length =  len(tmp_input)
-        print("start")
 
-        # Figure out if increasing or decreasing
-        if tmp_input[0] < tmp_input[1]:
-            increasing = True
-        elif tmp_input[0] > tmp_input[1]:
-            increasing = False
-        else:
-            # Should handle case when equal and skip this skip
-            print("equal found")
-            continue
-            
-        
-        # Check the level if valid
-        for idx in range(1, length):
-            valid = False
-            if (increasing and tmp_input[idx-1] < tmp_input[idx]) or (not increasing and tmp_input[idx-1] > tmp_input[idx]):
+        valid = False
+
+        if (tmp_input == sorted(tmp_input, reverse=False)) or (tmp_input == sorted(tmp_input, reverse=True)):
+             # Check the level is valid for min max difference
+             
+            tmp_count += 1 #Ignore this is for debugging only
+
+            for idx in range(1, length):
                 if 1 <= abs(tmp_input[idx-1] - tmp_input[idx]) <= 3:
                     valid = True
                 else:
                     valid = False
                     break
-            else:
-                valid = False
-                break
         if valid:
             count += 1
-            print(tmp_input[idx])
-        print("done")
+
+print(tmp_count)
 print(count)
