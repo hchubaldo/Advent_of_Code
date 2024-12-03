1757cc59d4e57483724cb7f1c7e5c2c7ae7c39de
hchubaldo <39108735+hchubaldo@users.noreply.github.com>
2024-12-03 12:02:57 -0600

    complete
    completed part 1 of day 2

--- hugo/DAY_2/day2_p1.py
+++ hugo/DAY_2/day2_p1.py
@@ -1,9 +1,17 @@
+def asc_desc(report, key = lambda x: x):
+    asc = all(key(report[i]) <= key(report[i + 1]) for i in range(len(report) - 1))
+    desc = all(key(report[i]) >= key(report[i + 1]) for i in range(len(report) - 1))
+    if asc or desc:
+        return True
+    else:
+        return False
+
 def problem_1(reports):
     count_safe = 0
     for report in reports:
         list_count = 1
         report_diffs = []
-        if (report == sorted(report, reverse=False)) or (report == sorted(report, reverse=True)):
+        if asc_desc(report, key = int):
             for num in report:
                 if list_count < len(report):
                     val_1 = num
@@ -23,7 +31,7 @@ def problem_1(reports):
 def main():
     reports = []
 
-    with open('/Users/hchubaldo/Documents/Advent_of_Coding/DAY_2_HHC/day2_input.txt', 'r') as f:
+    with open('/Users/hchubaldo/Documents/Advent_of_Coding/hugo/DAY_2/day2_input.txt', 'r') as f:
         for line in f.readlines():
             line = line.strip('\n').split(' ')
             reports.append(line)
