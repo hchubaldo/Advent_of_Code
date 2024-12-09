d62e5ffbb66c71a4373bfb461721e70277c3e3e7
hchubaldo <39108735+hchubaldo@users.noreply.github.com>
2024-12-09 14:58:49 -0600

    pusg
    pushing to save changes

--- hugo/DAY_3/day3_p2.py
+++ hugo/DAY_3/day3_p2.py
@@ -2,9 +2,12 @@ import regex as re
 
 def pattern_found(line):
     ans = 0
-    pattern = r"mul\((\d+,\d+)\)"
-    for nums in re.findall(pattern, line):
-        num1, num2 = map(int, nums.split(','))
+    pattern_remove = r"don't\(\).*?do\(\)"
+    line = re.sub(pattern_remove, '', line)
+    pattern_mul = r"mul\((\d+),(\d+)\)"
+    for nums in re.findall(pattern_mul, line):
+        print(nums)
+        num1, num2 = int(nums[0]), int(nums[1])
         mult_ans = num1 * num2
         ans += mult_ans
 
@@ -12,7 +15,6 @@ def pattern_found(line):
 
 def main():
     total = 0
-    
     with open('/Users/hchubaldo/Documents/Advent_of_Coding/hugo/DAY_3/day3_input.txt', 'r') as f:
         long_line = ' '.join([line.rstrip('\n') for line in f])
         total += pattern_found(long_line)
